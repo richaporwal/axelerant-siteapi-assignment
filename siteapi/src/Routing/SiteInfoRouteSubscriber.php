65141d1df9df9be022aa238b4676a3fe3271dd5e
richa <er.richa2012@gmail.com>
2019-08-26 10:58:39 +0530

    Deployed siteapi drupal8 custom module

---
+++ siteapi/src/Routing/SiteInfoRouteSubscriber.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Drupal\siteapi\Routing;
+
+use Drupal\Core\Routing\RouteSubscriberBase;
+use Symfony\Component\Routing\RouteCollection;
+
+/**
+ * Class SiteInfoRouteSubscriber.
+ *
+ * Listens to the dynamic route events.
+ */
+class SiteInfoRouteSubscriber extends RouteSubscriberBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function alterRoutes(RouteCollection $collection) {
+    if ($route = $collection->get('system.site_information_settings')) {
+      $route->setDefault('_form', 'Drupal\siteapi\Form\ExtendedSiteInformationForm');
+    }
+  }
+
+}
